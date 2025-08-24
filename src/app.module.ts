0f157910ffa719a5a13701551a35ad776a3f6f7a
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-08-24 16:26:28 -0400

    app mod clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,13 @@ import { StorageModule } from './module/storage/storage.module';
 import { LoggingModule } from './service/logger/logging.module';
 import { AuthModule } from './module/auth/auth.module';
 import { FileModule } from './module/file/file.module';
-import { SwaggerConfigService } from './config/swagger/swagger-config.service';
-import { ConfigController } from './controller/config/config.controller';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './module/user/user.module';
 import { GptModule } from './module/gpt/gpt.module';
 import { WorkflowModule } from './module/workflow/workflow.module';
 import { QuizModule } from './module/quiz/quiz.module';
-import { QuizController } from './module/quiz/quiz.controller';
 import { UserQuizResultModule } from './module/user-quiz-result/user-quiz-result.module';
-import { UserQuizResultController } from './module/user-quiz-result/user-quiz-result.controller';
+import { ConfigController } from './controller/config/config.controller';
 
 @Module({
   imports: [
@@ -33,12 +30,7 @@ import { UserQuizResultController } from './module/user-quiz-result/user-quiz-re
     QuizModule,
     UserQuizResultModule,
   ],
-  controllers: [
-    AppController,
-    ConfigController,
-    QuizController,
-    UserQuizResultController,
-  ],
-  providers: [AppService, SwaggerConfigService],
+  controllers: [AppController, ConfigController],
+  providers: [AppService],
 })
 export class AppModule {}
