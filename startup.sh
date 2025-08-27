e4414be8e8612810e0d74aeb52693f15469f69b5
Adam-Cox_goa <Adam.Cox@gov.ab.ca>
2025-08-27 12:16:53 -0400

    update path to chromium (will need to revisit during deploy to ACI)

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the browser path environment variable to the actual installation location
-export PLAYWRIGHT_BROWSER_PATH="/home/site/wwwroot/dist/.cache/ms-playwright/chromium_headless_shell-1187/chrome-linux/headless_shell"
+export PLAYWRIGHT_BROWSER_PATH="/home/.cache/ms-playwright/chromium_headless_shell-1187/chrome-linux/headless_shell"
 echo "Set PLAYWRIGHT_BROWSER_PATH to: $PLAYWRIGHT_BROWSER_PATH"
 
 # Start the application
